bd5b8abd7b4b983298b7b58564b6b8c4f3dc113c
jeffreybergier <jeffburg@jeffburg.com>
2017-05-07 15:11:36 -0700

    state restoration enabled
    this is super important. enabling it from moment 0.

--- WaterMe/WaterMe/AppDelegate.swift
+++ WaterMe/WaterMe/AppDelegate.swift
@@ -10,24 +10,35 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let window = self.window ?? UIWindow(frame: UIScreen.main.bounds)
         self.window = window
         
         let vc = UIViewController()
-
+        
         window.rootViewController = vc
         window.backgroundColor = .white
         window.makeKeyAndVisible()
         
+        return true
+    }
+    
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        let _savedBuild = coder.decodeObject(forKey: UIApplicationStateRestorationBundleVersionKey) as? String
+        let _currentBuild = Bundle(for: type(of: self)).infoDictionary?[kCFBundleVersionKey as String] as? String
+        guard let savedBuild = _savedBuild, let currentBuild = _currentBuild, currentBuild == savedBuild else { return false }
         return true
     }
 }
