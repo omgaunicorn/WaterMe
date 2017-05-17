2a0d4cc47dcc090e48ad731eff1390d38a7144d8
jeffreybergier <jeffburg@jeffburg.com>
2017-05-16 19:18:54 -0700

    started doing boot sequence

--- WaterMe/WaterMe/Router/RouterViewController.swift
+++ WaterMe/WaterMe/Router/RouterViewController.swift
@@ -37,13 +37,67 @@ import UIKit
     NO (subscription not present) - Assume new user - setup for local free realm
 */
 
-class RouterViewController: UIViewController {
+class RouterViewController: UIViewController, HasRealmControllers {
     
-    class func newVC() -> RouterViewController {
+    class func newVC(basicRealmController: RealmController? = nil, proRealmController: RealmController? = nil) -> RouterViewController {
         let sb = UIStoryboard(name: "Router", bundle: Bundle(for: self))
         // swiftlint:disable:next force_cast
         let vc = sb.instantiateInitialViewController() as! RouterViewController
+        vc.configure(withBasic: basicRealmController, andPro: proRealmController)
         return vc
+    }
+    
+    var basicRealmController: RealmController!
+    var proRealmController: RealmController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.basicRealmController == nil {
+            self.startBootSequence()
+        }
+    }
+    
+    private func startBootSequence() {
+        // Eventual Boot Sequence
+        /*
+         1. Check for Core Data database
+            YES - migrate to free realm, present new subscription options
+            NO - Go to 2
+         
+         2. Check realm for icloud logged in user
+            YES - Check the Basic Realm receipt object to see if there is a realm for Pro photo sync
+                YES - Load pro data assuming the user is Pro
+                NO - Load app assuming user has Basic cloud sync
+            NO - Go to 3
+         
+         3. Check if there is a local realm on disk
+            YES - Assume user is free and load app
+            NO - Go to 4
+         
+         4. Check if there is a receipt
+            NO - Assume new free user - create local realm
+            YES - Go to 5
+         
+         5. Read receipt and check for subscription
+            YES (subscription present) - Assume receipt is valid, configure realm for subscription level
+            NO (subscription not present) - Assume new user - setup for local free realm
+        */
+        
+        if let alreadyLoggedInUser = RealmController.loggedInUser {
+            let basicRC = RealmController(kind: .basic(alreadyLoggedInUser))
+            var proRC: RealmController?
+            // if basicRC.reciept.pro == true {
+            //    proRC = RealmController(kind: .pro(alreadyLoggedInUser))
+            // }
+            self.configure(withBasic: basicRC, andPro: proRC)
+        } else if RealmController.localRealmExists == true {
+            let freeRC = RealmController(kind: .local)
+            self.configure(withBasic: freeRC, andPro: nil)
+        } else {
+            // check receipt
+            let freeRC = RealmController(kind: .local)
+            self.configure(withBasic: freeRC, andPro: nil)
+        }
     }
     
     @IBAction private func premiumButtonTapped(_ sender: NSObject?) {
--- WaterMe/WaterMeData/Source/RealmController.swift
+++ WaterMe/WaterMeData/Source/RealmController.swift
@@ -9,9 +9,33 @@
 import CloudKit
 import RealmSwift
 
-public protocol HasRealmControllers {
-    
+public protocol HasRealmControllers: class {
+    var basicRealmController: RealmController! { get set }
+    var proRealmController: RealmController? { get set }
 }
+
+public extension HasRealmControllers {
+    public func configure(withBasic basic: RealmController?, andPro pro: RealmController?) {
+        if let basic = basic {
+            switch basic.kind {
+            case .basic, .local:
+                self.basicRealmController = basic
+            case .pro:
+                fatalError("Pro RealmController passed in as Non-Pro Controller")
+            }
+        }
+        if let pro = pro {
+            switch pro.kind {
+            case .pro:
+                self.proRealmController = pro
+            case .local, .basic:
+                fatalError("Non-Pro RealmController passed in as Pro Controller")
+            }
+        }
+    }
+}
+
+
 
 public class RealmController {
     
