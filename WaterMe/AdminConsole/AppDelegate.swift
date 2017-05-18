2507a218647f2714a6875e6738dbc10e91ee9857
jeffreybergier <jeffburg@jeffburg.com>
2017-05-18 16:01:52 -0700

    added admin console

---
+++ WaterMe/AdminConsole/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AdminConsole
+//
+//  Created by Jeffrey Bergier on 5/18/17.
+//  Copyright © 2017 Saturday Apps. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ WaterMe/AdminConsole/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AdminConsole
+//
+//  Created by Jeffrey Bergier on 5/18/17.
+//  Copyright © 2017 Saturday Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
