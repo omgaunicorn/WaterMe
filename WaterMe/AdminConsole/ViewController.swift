1b0665f0dcf14603e5ac4a558fefd6d366ef4712
jeffreybergier <jeffburg@jeffburg.com>
2017-05-19 10:01:42 -0700

    ready to start parsing json response

--- WaterMe/AdminConsole/ViewController.swift
+++ WaterMe/AdminConsole/ViewController.swift
@@ -15,28 +15,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let user = SyncUser.current {
-            self.doRealm(user: user)
-        } else {
-            let credentials = SyncCredentials.usernamePassword(username: PrivateKeys.kRealmAdminLogin, password: PrivateKeys.kRealmAdminPassword, register: false)
-            SyncUser.logIn(with: credentials, server: WaterMeData.PrivateKeys.kRealmServer) { user, error in
-                DispatchQueue.main.async {
-                    guard let user = user else { print(error!); return; }
-                    self.doRealm(user: user)
-                }
-            }
-        }
-    }
-    
-    var realmController: RealmController?
-
-    private func doRealm(user: SyncUser) {
-        let kind = RealmController.Kind.basic(user)
-        let realmController = RealmController(kind: kind, overrideUserPath: "a585e812c45049b8aaaba33797ab7767/")
-        self.realmController = realmController
-        realmController.receiptChanged = { receipt in
-            print(receipt)
-        }
+        let data = PrivateKeys.jsonSampleData.data(using: .utf8)!
+        let json = try! JSONSerialization.jsonObject(with: data, options: [])
+        print(json)
     }
 }
 
