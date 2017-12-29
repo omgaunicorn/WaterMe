a5a1177889ac59479bdcfbb08b2e7c0912d71b3f
Jeffrey Bergier <jeffburg@jeffburg.com>
2017-12-29 16:54:06 -0500

    got old core data info loading

--- WaterMe/WaterMe/CoreDataMigration/CoreDataMigrator.swift
+++ WaterMe/WaterMe/CoreDataMigration/CoreDataMigrator.swift
@@ -21,13 +21,48 @@
 //  along with WaterMe.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+import CoreData
 import Foundation
 import WaterMeData
 
+class WaterMePersistentContainer: NSPersistentContainer {
+    override class func defaultDirectoryURL() -> URL {
+        return CoreDataMigrator.storeDirectory
+    }
+}
+
 class CoreDataMigrator {
 
+    static let storeDirectory: URL = {
+        let appSupport = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        let sqLiteURL = appSupport.appendingPathComponent("WaterMe", isDirectory: true)
+        return sqLiteURL
+    }()
+
+    static let storeURL: URL = {
+        return CoreDataMigrator.storeDirectory.appendingPathComponent("WaterMeData.sqlite")
+    }()
+
+    private let container: NSPersistentContainer
+
     init?() {
-        return nil
+        let sqLiteURL = CoreDataMigrator.storeURL
+        // if the file doesn't exist then they never had the old version of the app.
+        guard FileManager.default.fileExists(atPath: sqLiteURL.path) == true else { return nil }
+        let container = WaterMePersistentContainer(name: "WaterMeData")
+        container.persistentStoreDescriptions.first?.isReadOnly = true
+        container.loadPersistentStores() { description, error  in
+            guard error == nil else {
+                let error = "Error Loading Core Data Model. This leaves the Migrator in an invalid state: \(error!)"
+                log.error(error)
+                assertionFailure(error)
+                return
+            }
+            log.debug("Loaded Core Data Stack: Ready for Migration.")
+            let stuff = try! container.viewContext.fetch(NSFetchRequest(entityName: String(describing: PlantEntity.self)))
+            print(stuff)
+        }
+        self.container = container
     }
 
 }
