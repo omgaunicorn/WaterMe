1489ef8ffaa7c852edd518f387cf3ca7dabc8330
Jeffrey Bergier <jeffburg@jeffburg.com>
2018-09-07 10:49:02 -0700

    got arrow working in all 4 directions

--- WaterMe/WaterMe/ReminderMain/ReminderSummary/ReminderSummaryPopoverBackgroundView.m
+++ WaterMe/WaterMe/ReminderMain/ReminderSummary/ReminderSummaryPopoverBackgroundView.m
@@ -29,13 +29,14 @@
     UIPopoverArrowDirection _arrowDirection;
 }
 
-@property (nonatomic, weak) UIView*_Nullable myArrowView;
-@property (nonatomic, weak) NSLayoutConstraint*_Nullable horizontalArrowConstraint;
-@property (nonatomic, weak) NSLayoutConstraint*_Nullable verticalArrowConstraint;
-@property (nonatomic, strong) NSArray<NSLayoutConstraint*>*_Nonnull upConstraints;
-@property (nonatomic, strong) NSArray<NSLayoutConstraint*>*_Nonnull downConstraints;
-@property (nonatomic, strong) NSArray<NSLayoutConstraint*>*_Nonnull leadingConstraints;
-@property (nonatomic, strong) NSArray<NSLayoutConstraint*>*_Nonnull trailingConstraints;
+@property (nonatomic, weak, nullable) UIView* myArrowView;
+@property (nonatomic, weak, nullable) CALayer* myArrowViewMaskLayer;
+@property (nonatomic, weak, nullable) NSLayoutConstraint* horizontalArrowConstraint;
+@property (nonatomic, weak, nullable) NSLayoutConstraint* verticalArrowConstraint;
+@property (nonatomic, strong, nonnull) NSArray<NSLayoutConstraint*>* upConstraints;
+@property (nonatomic, strong, nonnull) NSArray<NSLayoutConstraint*>* downConstraints;
+@property (nonatomic, strong, nonnull) NSArray<NSLayoutConstraint*>* leadingConstraints;
+@property (nonatomic, strong, nonnull) NSArray<NSLayoutConstraint*>* trailingConstraints;
 
 @end
 
@@ -45,17 +46,17 @@
 
 // MARK: Required of Implementers
 
-+ (CGFloat)arrowBase;
-{
-    return 40;
-}
 + (UIEdgeInsets)contentViewInsets;
 {
     return UIEdgeInsetsMake(0, 0, 0, 0);
 }
 + (CGFloat)arrowHeight;
 {
-    return 40;
+    return 30;
+}
++ (CGFloat)arrowBase;
+{
+    return 50;
 }
 + (BOOL)wantsDefaultContentAppearance;
 {
@@ -91,31 +92,41 @@
 {
     [super didMoveToWindow];
     if (![self myArrowView]) {
-        [self configureMyArrowView];
-        [self enableDisableConstraintsForArrowDirection];
+        [self configureArrowViewConstraints];
         [self updateArrowConstraintsForOffset];
+        [self configureArrowMaskLayer];
+        [self enableDisableConstraintsForArrowDirection];
     }
     [[self layer] setBorderColor:[[UIColor blackColor] CGColor]];
     [[self layer] setBorderWidth:1];
 }
 
-- (void)configureMyArrowView;
+- (void)configureArrowViewConstraints;
 {
+    // Create and Add the View
     UIVisualEffect* effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
     UIView* view = [[UIVisualEffectView alloc] initWithEffect:effect];
     [self setMyArrowView:view];
     [view setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self addSubview:view];
-    [self addConstraints:@[
-                           [[view widthAnchor] constraintEqualToConstant:40],
-                           [[view heightAnchor] constraintEqualToConstant:40],
-                           ]];
-    NSLayoutConstraint* upDownSlide = [[view centerXAnchor] constraintEqualToAnchor:[self centerXAnchor]];
-    NSLayoutConstraint* leftRightSlide = [[view centerYAnchor] constraintEqualToAnchor:[self centerYAnchor]];
-    NSLayoutConstraint* upAttach = [[view topAnchor] constraintEqualToAnchor:[self topAnchor]];
-    NSLayoutConstraint* downAttach = [[view bottomAnchor] constraintEqualToAnchor:[self bottomAnchor]];
-    NSLayoutConstraint* leadAttach = [[view leadingAnchor] constraintEqualToAnchor:[self leadingAnchor]];
-    NSLayoutConstraint* trailAttach = [[view trailingAnchor] constraintEqualToAnchor:[self trailingAnchor]];
+
+    // Configure height and width constraints
+    // And add them. These never change
+    NSLayoutConstraint* arrowWidthConstraint = [[view widthAnchor] constraintEqualToConstant:[self aB]];
+    NSLayoutConstraint* arrowHeightConstraint = [[view heightAnchor] constraintEqualToConstant:[self aH]];
+    [self addConstraints:@[arrowWidthConstraint, arrowHeightConstraint]];
+
+    // Configure left/right/top/bottom attachments
+    // But don't add them, these change
+    NSLayoutConstraint* upDownSlide = [[view centerXAnchor] constraintEqualToAnchor:[self centerXAnchor] constant:0];
+    NSLayoutConstraint* leftRightSlide = [[view centerYAnchor] constraintEqualToAnchor:[self centerYAnchor] constant:0];
+    NSLayoutConstraint* upAttach = [[view topAnchor] constraintEqualToAnchor:[self topAnchor] constant:0];
+    NSLayoutConstraint* downAttach = [[view bottomAnchor] constraintEqualToAnchor:[self bottomAnchor] constant:0];
+    NSLayoutConstraint* leadAttach = [[view leadingAnchor] constraintEqualToAnchor:[self leadingAnchor] constant:0];
+    NSLayoutConstraint* trailAttach = [[view trailingAnchor] constraintEqualToAnchor:[self trailingAnchor] constant:0];
+
+    // Store them in the appropriate properties
+    // These need to change dynamically
     [self setUpConstraints:@[upDownSlide, upAttach]];
     [self setDownConstraints:@[upDownSlide, downAttach]];
     [self setLeadingConstraints:@[leadAttach, leftRightSlide]];
@@ -133,21 +144,32 @@
     [self removeConstraints:[self downConstraints]];
     [self removeConstraints:[self leadingConstraints]];
     [self removeConstraints:[self trailingConstraints]];
+    CGAffineTransform leftRightRotation;
+    CGAffineTransform leftRightRotationAndTranslation;
     switch ([self arrowDirection]) {
         case UIPopoverArrowDirectionAny:
         case UIPopoverArrowDirectionUnknown:
-            assert(NO);
-            break;
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:@"Unexpected Arrow Direction"
+                                         userInfo:nil];
         case UIPopoverArrowDirectionUp:
+            [[self myArrowView] setTransform:CGAffineTransformIdentity];
             [self addConstraints:[self upConstraints]];
             break;
         case UIPopoverArrowDirectionDown:
+            [[self myArrowView] setTransform:CGAffineTransformMakeRotation((180.0 * M_PI) / 180.0)];
             [self addConstraints:[self downConstraints]];
             break;
         case UIPopoverArrowDirectionLeft:
+            leftRightRotation = CGAffineTransformMakeRotation((-90 * M_PI) / 180.0);
+            leftRightRotationAndTranslation = CGAffineTransformTranslate(leftRightRotation, 0, ([self aH] - [self aB])/2);
+            [[self myArrowView] setTransform:leftRightRotationAndTranslation];
             [self addConstraints:[self leadingConstraints]];
             break;
         case UIPopoverArrowDirectionRight:
+            leftRightRotation = CGAffineTransformMakeRotation((90 * M_PI) / 180.0);
+            leftRightRotationAndTranslation = CGAffineTransformTranslate(leftRightRotation, 0, ([self aH] - [self aB])/2);
+            [[self myArrowView] setTransform:leftRightRotationAndTranslation];
             [self addConstraints:[self trailingConstraints]];
             break;
     }
@@ -157,6 +179,39 @@
 {
     [[self horizontalArrowConstraint] setConstant:[self arrowOffset]];
     [[self verticalArrowConstraint] setConstant:[self arrowOffset]];
+}
+
+- (void)configureArrowMaskLayer;
+{
+    [[self myArrowViewMaskLayer] removeFromSuperlayer];
+    [self setMyArrowViewMaskLayer:nil];
+    UIBezierPath* path = [[UIBezierPath alloc] init];
+    [path moveToPoint:CGPointMake([self aG], [self aH])];
+    [path addLineToPoint:CGPointMake([self aB] / 2, [self aG])];
+    [path addLineToPoint:CGPointMake([self aB], [self aH])];
+    [path closePath];
+    CAShapeLayer* mask = [[CAShapeLayer alloc] init];
+    [mask setPath:[path CGPath]];
+    [mask setFillColor:[[UIColor blackColor] CGColor]];
+    [mask setFillRule:kCAFillRuleNonZero];
+    [[[self myArrowView] layer] setMask:mask];
+    [self setMyArrowViewMaskLayer:mask];
+    [mask setPosition: [[[self myArrowView] layer] position]];
+}
+
+- (CGFloat)aG;
+{
+    return 2;
+}
+
+- (CGFloat)aH;
+{
+    return [ReminderSummaryPopoverBackgroundView arrowHeight] - [self aG];
+}
+
+- (CGFloat)aB;
+{
+    return [ReminderSummaryPopoverBackgroundView arrowBase] - [self aG];
 }
 
 @end
