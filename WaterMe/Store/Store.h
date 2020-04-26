df75de7b4cb53da993c950adcefad33ba94a4f47
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-04-26 11:40:05 +0900

    [ReducePods] Added Datum/Store Frameworks

---
+++ WaterMe/Store/Store.h
@@ -0,0 +1,19 @@
+//
+//  Store.h
+//  Store
+//
+//  Created by Jeffrey Bergier on 2020/04/26.
+//  Copyright © 2020 Saturday Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Store.
+FOUNDATION_EXPORT double StoreVersionNumber;
+
+//! Project version string for Store.
+FOUNDATION_EXPORT const unsigned char StoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Store/PublicHeader.h>
+
+
