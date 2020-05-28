f8d0261e395239dd7f6807a16f469fd7e573182c
Jeffrey Bergier <jbergier@mercari.com>
2020-05-28 10:54:54 +0900

    [Core Data] Fixed framework tests

--- WaterMe/Frameworks/Datum/DatumTests/DatumTests.swift
+++ WaterMe/Frameworks/Datum/DatumTests/DatumTests.swift
@@ -9,26 +9,25 @@
 import XCTest
 @testable import Datum
 
-class DatumTests: XCTestCase {
+class DatumTestsBase: XCTestCase {
+    
+    class func newBasicController() -> BasicController {
+//        fatalError("Subclass to provide Realm or CD Basic Controller for testing")
+        return try! testing_NewRLMBasicController(of: .local).get()
+    }
+    
+    private(set) var basicController: BasicController!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.basicController = type(of: self).newBasicController()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test123() {
+        print("test")
     }
 
 }
