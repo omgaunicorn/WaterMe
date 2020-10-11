cc066645cdbbbe817b59d067c30f07fcee83a29c
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-10-11 21:08:28 +0900

    Revert "[CD] Get LOG email when migration succeeds"
    This reverts commit 949912f1dab19d164bbb58a8aed76fce2c038b96.

--- WaterMe/Frameworks/Datum/Datum/Migration/RealmToCoreDataMigrator.swift
+++ WaterMe/Frameworks/Datum/Datum/Migration/RealmToCoreDataMigrator.swift
@@ -162,7 +162,7 @@ internal class RealmToCoreDataMigrator: Migratable {
                     // Cleanup source, this HAS to work for migration to finish
                     do {
                         try FileManager.default.removeItem(at: RLM_BasicController.storeDirectoryURL)
-                        "Migration Succeeded".log(as: .warning)
+                        "Migration Succeeded".log(as: .info)
                     } catch {
                         "Migration Error: Succeeded but failed to delete Realm DB: \(error)".log()
                         completed = false
