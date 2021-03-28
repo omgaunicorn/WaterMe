3492f957ff48ba959b3dd65913baac971fae3747
jeffreybergier <jeffburg@jeffburg.com>
2021-03-28 11:54:27 +0900

    [CloudKit] added sync progress

---
+++ WaterMe/Frameworks/Calculate/Calculate/CloudKitContainerContinuousProgress.swift
@@ -0,0 +1,151 @@
+//
+//  Created by Jeffrey Bergier on 2021/01/28.
+//
+//  MIT License
+//
+//  Copyright (c) 2021 Jeffrey Bergier
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import SwiftUI
+import CoreData
+import Combine
+import CloudKit
+
+// TODO: Replace this file with original from Umbrella library
+
+// Highly inspired by
+// https://github.com/ggruen/CloudKitSyncMonitor/blob/main/Sources/CloudKitSyncMonitor/SyncMonitor.swift
+
+/// Shows continuous progress of CloudKit syncing via NSPersistentCloudKitContainer.
+/// Note, this class is not tested because it relies on NSNotificationCenter and other singletons.
+@available(iOS 14.0, OSX 11.0, *)
+public class CloudKitContainerContinuousProgress: ContinousProgress {
+    
+    public enum Error: UserFacingError {
+                
+        case accountStatusCritical(NSError)
+        case accountStatus(CKAccountStatus)
+        case sync(NSError)
+        
+        public var errorCode: Int {
+            switch self {
+            case .accountStatusCritical:
+                return 1001
+            case .accountStatus:
+                return 1002
+            case .sync:
+                return 1003
+            }
+        }
+        public var isCritical: Bool { false }
+        public var recoveryActions: [RecoveryAction] { [.openWaterMeSettings] }
+        public var title: String? { "Noun.iCloud" }
+        public var message: String? {
+            switch self {
+            case .accountStatus:
+                return "Phrase.ErroriCloudAccount"
+            case .accountStatusCritical(let error), .sync(let error):
+                return .init(error.localizedDescription)
+            }
+        }
+    }
+    
+    public var initializeError: UserFacingError?
+    public let progress: Progress
+    public var errorQ = ErrorQueue()
+    
+    private let syncName = NSPersistentCloudKitContainer.eventChangedNotification
+    private let accountName = Notification.Name.CKAccountChanged
+    private var io: Set<UUID> = []
+    
+    /// If container is not NSPersistentCloudKitContainer this class never shows any progress.
+    public init(_ container: NSPersistentContainer) {
+        self.progress = .init(totalUnitCount: 0)
+        self.progress.completedUnitCount = 0
+        guard container is NSPersistentCloudKitContainer else {
+            "CloudKitContainerContinuousProgress can only be show progress of sync with NSPersistentCloudKitContainer".log(as: .severe)
+            return
+        }
+        let nc = NotificationCenter.default
+        nc.addObserver(self,
+                       selector: #selector(self.observeSync(_:)),
+                       name: self.syncName,
+                       object: container)
+        nc.addObserver(self,
+                       selector: #selector(self.observeAccount),
+                       name: self.accountName,
+                       object: nil)
+        self.observeAccount()
+    }
+    
+    @objc private func observeAccount() {
+        CKContainer.default().accountStatus() { account, error in
+            DispatchQueue.main.async {
+                self.objectWillChange.send()
+                if let error = error {
+                    error.log()
+                    let error = error as NSError
+                    self.initializeError = Error.accountStatusCritical(error)
+                    return
+                }
+                switch account {
+                case .available:
+                    self.initializeError = nil
+                case .couldNotDetermine, .restricted, .noAccount:
+                    fallthrough
+                @unknown default:
+                    self.initializeError = Error.accountStatus(account)
+                }
+            }
+        }
+    }
+    
+    @objc private func observeSync(_ aNotification: Notification) {
+        let key = NSPersistentCloudKitContainer.eventNotificationUserInfoKey
+        guard let event = aNotification.userInfo?[key]
+                as? NSPersistentCloudKitContainer.Event else { return }
+        DispatchQueue.main.async {
+            self.objectWillChange.send()
+            if let error = event.error {
+                error.log()
+                let error = error as NSError
+                self.errorQ.append(Error.sync(error))
+            }
+            if self.io.contains(event.identifier) {
+                "- \(event.identifier)".log(as: .debug)
+                self.io.remove(event.identifier)
+                self.progress.completedUnitCount += 1
+            } else {
+                "+ \(event.identifier)".log(as: .debug)
+                self.io.insert(event.identifier)
+                self.progress.totalUnitCount += 1
+            }
+            "progress: \(self.progress.completedUnitCount) / \(self.progress.totalUnitCount)".log(as: .debug)
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: self.syncName, object: nil)
+        NotificationCenter.default.removeObserver(self, name: self.accountName, object: nil)
+        Loggable.default?.verbose()
+    }
+}
+
---
+++ WaterMe/Frameworks/Calculate/Calculate/ContinuousProgress.swift
@@ -0,0 +1,80 @@
+//
+//  Created by Jeffrey Bergier on 2021/01/28.
+//
+//  MIT License
+//
+//  Copyright (c) 2021 Jeffrey Bergier
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Combine
+import CoreData
+
+
+// TODO: Replace this file with original from Umbrella library
+
+public typealias ErrorQueue = [UserFacingError]
+
+/// Represents the progress of something that is long running and can produce
+/// errors upon startup and errors while running. I use this to reflect the status
+/// of NSPersistentCloudKitContainer sync. But its generic enough to be used for
+/// many types of long-running / background processes.
+/// Use `AnyContinousProgress` to get around AssociatedType compile errors.
+public protocol ContinousProgress: ObservableObject {
+    /// Fixed error that only occurs on startup and doesn't change
+    /// for the lifetime of the process.
+    var initializeError: UserFacingError? { get }
+    var progress: Progress { get }
+    /// When an error occurs, append it to the Queue.
+    var errorQ: ErrorQueue { get set }
+}
+
+@available(iOS 13.0, *)
+public class AnyContinousProgress: ContinousProgress {
+    
+    public let objectWillChange: ObservableObjectPublisher
+    public var initializeError: UserFacingError? { _initializeError() }
+    public var progress: Progress { _progress() }
+    public var errorQ: ErrorQueue {
+        get { _errorQ_get() }
+        set { _errorQ_set(newValue) }
+    }
+    
+    private var _initializeError: () -> UserFacingError?
+    private var _progress: () -> Progress
+    private var _errorQ_get: () -> ErrorQueue
+    private var _errorQ_set: (ErrorQueue) -> Void
+    
+    public init<T: ContinousProgress>(_ progress: T) where T.ObjectWillChangePublisher == ObservableObjectPublisher {
+        self.objectWillChange = progress.objectWillChange
+        _initializeError      = { progress.initializeError }
+        _progress             = { progress.progress }
+        _errorQ_get           = { progress.errorQ }
+        _errorQ_set           = { progress.errorQ = $0 }
+    }
+}
+
+/// Use when you have no progress to report
+public class NoContinousProgress: ContinousProgress {
+    public let initializeError: UserFacingError? = nil
+    public let progress: Progress = .init()
+    public var errorQ: ErrorQueue = .init()
+    public init() {}
+}
