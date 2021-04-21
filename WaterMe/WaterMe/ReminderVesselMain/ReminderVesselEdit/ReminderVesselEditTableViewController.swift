a80464be9680f1970d3dc45c6d7cddaa6dc878da
jeffreybergier <jeffburg@jeffburg.com>
2021-04-21 21:29:31 +0900

    [Pause] Added confirm on delete reminder by swipe

--- WaterMe/WaterMe/ReminderVesselMain/ReminderVesselEdit/ReminderVesselEditTableViewController.swift
+++ WaterMe/WaterMe/ReminderVesselMain/ReminderVesselEdit/ReminderVesselEditTableViewController.swift
@@ -259,12 +259,27 @@ class ReminderVesselEditTableViewController: StandardTableViewController {
             let deleteAction = UIContextualAction(style: .destructive,
                                                   title: UIAlertController.LocalizedString.buttonTitleDelete)
             { [unowned self] _, _, successfullyDeleted in
-                guard let reminder = self.remindersData?[indexPath.row] else {
-                    successfullyDeleted(false)
-                    return
+                // TODO: Improve this
+                // This causes bug when swiping to delete where popover
+                // on iPAd is pointed way over outside the table/
+                // let cell = tableView.cellForRow(at: indexPath)
+                // let sender = cell.map { PopoverSender.left($0) }
+                let confirmation = UIAlertController(localizedDeleteConfirmationWithOptions: [],
+                                                     sender: nil)
+                { confirmed in
+                    switch confirmed {
+                    case .cancel, .pause:
+                        successfullyDeleted(false)
+                    case .delete:
+                        guard let reminder = self.remindersData?[indexPath.row] else {
+                            successfullyDeleted(false)
+                            return
+                        }
+                        let deleted = self.delegate?.userDeleted(reminder: reminder, controller: self) ?? false
+                        successfullyDeleted(deleted)
+                    }
                 }
-                let deleted = self.delegate?.userDeleted(reminder: reminder, controller: self) ?? false
-                successfullyDeleted(deleted)
+                self.present(confirmation, animated: true, completion: nil)
             }
 
             let pauseAction = UIContextualAction(style: .normal,
