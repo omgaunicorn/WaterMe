da5cd2ea51e899eaa4e0fd0b600d27e1b136f647
jeffreybergier <jeffburg@jeffburg.com>
2021-05-03 08:17:54 +0900

    [Pause] Added files to test core data schema migration

---
+++ WaterMe/Frameworks/Datum/DatumTests/SchemaMigration/SchemaMigrationTests.swift
@@ -0,0 +1,30 @@
+//
+//  SchemaMigrationTests.swift
+//  DatumTests
+//
+//  Created by Jeffrey Bergier on 2021/05/03.
+//  Copyright © 2021 Saturday Apps. All rights reserved.
+//
+//  This file is part of WaterMe.  Simple Plant Watering Reminders for iOS.
+//
+//  WaterMe is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  WaterMe is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with WaterMe.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+@testable import Datum
+import CoreData
+import XCTest
+
+class CoreDataSchemaMigrationTests: XCTestCase {
+    
+}
