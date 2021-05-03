db2ebb092fc4f013d5795c0534c4596ded219083
jeffreybergier <jeffburg@jeffburg.com>
2021-05-03 09:01:15 +0900

    [Pause] Added first basic migration test

--- WaterMe/Frameworks/Datum/DatumTests/SchemaMigration/SchemaMigrationTests.swift
+++ WaterMe/Frameworks/Datum/DatumTests/SchemaMigration/SchemaMigrationTests.swift
@@ -25,6 +25,51 @@
 import CoreData
 import XCTest
 
+fileprivate let TestDB1URL     = Bundle(for: NonSyncContainer.self)
+                                       .url(forResource: "TestDB", withExtension: "sqlite")!
+fileprivate let TestDB2URL     = Bundle(for: NonSyncContainer.self)
+                                       .url(forResource: "TestDB", withExtension: "sqlite-wal")!
+fileprivate let StoreParentURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+                                    .appendingPathComponent(UUID().uuidString, isDirectory: true)
+fileprivate let Store1URL      = StoreParentURL.appendingPathComponent("WaterMe.sqlite", isDirectory: false)
+fileprivate let Store2URL      = StoreParentURL.appendingPathComponent("WaterMe.sqlite-wal", isDirectory: false)
+
+fileprivate class NonSyncContainer: NSPersistentContainer {
+    override class func defaultDirectoryURL() -> URL {
+        return StoreParentURL
+    }
+}
+
+fileprivate class SyncContainer: NSPersistentContainer {
+    override class func defaultDirectoryURL() -> URL {
+        return StoreParentURL
+    }
+}
+
 class CoreDataSchemaMigrationTests: XCTestCase {
+        
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        let fm = FileManager.default
+        try fm.createDirectory(at: StoreParentURL,
+                               withIntermediateDirectories: true,
+                               attributes: nil)
+        try fm.copyItem(at: TestDB1URL, to: Store1URL)
+        try fm.copyItem(at: TestDB2URL, to: Store2URL)
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        let fm = FileManager.default
+        try fm.removeItem(at: StoreParentURL)
+    }
+    
+    func test_nonSync_initialize_simpleMigration() throws {
+        _ = try CD_BasicController(kind: .__testing_withClass(NonSyncContainer.self))
+    }
+    
+    func test_sync_initialize_simpleMigration() throws {
+        _ = try CD_BasicController(kind: .__testing_withClass(SyncContainer.self))
+    }
     
 }
